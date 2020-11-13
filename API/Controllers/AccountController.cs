5a9e4ab8e1d47d4ec5d18d5205e57bde5127ef37
Sumit1984 <sumit.jinaral@gmail.com>
2020-11-13 14:46:43 +0530

    User Repository and repository pattern

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -48,7 +48,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
-                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+                .FirstOrDefaultAsync(x => x.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized();
 
